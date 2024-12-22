7c77c5bd3c0cde2884fd6c8d1aad5a3c27688836
ugur9384 <ugursozer77@gmail.com>
2024-12-22 11:18:46 +0300

    Update navlinks.js

--- navlinks.js
+++ navlinks.js
@@ -2,17 +2,17 @@
 fetch('navlinks.html')
   .then(response => response.text())
   .then(data => {
-    // Insert the fetched navigation links into the
+   
     const navElement = document.querySelector('header nav');
     navElement.innerHTML = data;
 
-    // Highlight the active link based on the current URL
-    const currentPath = window.location.pathname; // Get the current page's path
-    const links = navElement.querySelectorAll('a'); // Get all <a> tags in the navigation
+    
+    const currentPath = window.location.pathname;
+    const links = navElement.querySelectorAll('a');
 
     links.forEach(link => {
       if (link.getAttribute('href') === currentPath || currentPath.endsWith(link.getAttribute('href'))) {
-        link.classList.add('active'); // Add the 'active' class to the matching link
+        link.classList.add('active'); 
       }
     });
   })
